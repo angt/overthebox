5b57ddd2f47536aaf368cca24b77c2b72087592f
Adrien Gallouët <adrien@gallouet.fr>
2019-09-28 07:15:25 +0000

    wip
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- build.sh
+++ build.sh
@@ -5,20 +5,6 @@ set -e
 umask 0022
 unset GREP_OPTIONS SED
 
-_get_repo() (
-	mkdir -p "$1"
-	cd "$1"
-	[ -d .git ] || git init
-	if git remote get-url origin >/dev/null 2>/dev/null; then
-		git remote set-url origin "$2"
-	else
-		git remote add origin "$2"
-	fi
-	git fetch origin
-	git fetch origin --tags
-	git checkout "origin/$3" -B "build" 2>/dev/null || git checkout "$3" -B "build"
-)
-
 OTB_HOST=${OTB_HOST:-$(curl -sS ipaddr.ovh)}
 OTB_PORT=${OTB_PORT:-8000}
 OTB_REPO=${OTB_REPO:-http://$OTB_HOST:$OTB_PORT/$OTB_PATH}
@@ -27,8 +13,7 @@ OTB_TARGET=${OTB_TARGET:-x86_64}
 OTB_CONFIG=${OTB_CONFIG:-net-full nice-bb usb-full legacy}
 OTB_PKGS=${OTB_PKGS:-vim-full netcat htop iputils-ping bmon bwm-ng screen mtr ss strace tcpdump-mini ethtool sysstat pciutils mini_snmpd dmesg}
 
-OTB_FEED_URL="${OTB_FEED_URL:-https://github.com/ovh/overthebox-feeds}"
-OTB_FEED_SRC="${OTB_FEED_SRC:-master}"
+OTB_FEED=${OTB_FEED:-https://github.com/ovh/overthebox-feeds;master}
 
 for i in $OTB_TARGET $OTB_CONFIG; do
 	if [ ! -f "config/$i" ]; then
@@ -37,22 +22,15 @@ for i in $OTB_TARGET $OTB_CONFIG; do
 	fi
 done
 
-OTB_FEED_BRANCH="openwrt-18.06@{2019-04-24 00:00:00}"
-
-_get_repo source https://github.com/ovh/overthebox-lede "otb-openwrt-18.06-19.04.24"
-_get_repo feeds/packages https://github.com/openwrt/packages "$OTB_FEED_BRANCH"
-_get_repo feeds/luci https://github.com/openwrt/luci "$OTB_FEED_BRANCH"
-_get_repo feeds/routing https://github.com/openwrt-routing/packages "$OTB_FEED_BRANCH"
-
 if [ -z "$OTB_FEED" ]; then
 	OTB_FEED=feeds/overthebox
 	_get_repo "$OTB_FEED" "$OTB_FEED_URL" "$OTB_FEED_SRC"
 fi
 
-rm -rf source/bin source/files source/tmp
-cp -rf root source/files
+rm -rf openwrt/bin openwrt/files openwrt/tmp
+cp -rf root openwrt/files
 
-cat >> source/files/etc/banner <<EOF
+cat >> openwrt/files/etc/banner <<EOF
 -----------------------------------------------------
  VERSION:     $(git describe --tag --always)
 
@@ -61,14 +39,14 @@ cat >> source/files/etc/banner <<EOF
 -----------------------------------------------------
 EOF
 
-cat > source/feeds.conf <<EOF
+cat > openwrt/feeds.conf <<EOF
 src-link packages $(readlink -f feeds/packages)
 src-link luci $(readlink -f feeds/luci)
 src-link routing $(readlink -f feeds/routing)
-src-link overthebox $(readlink -f "$OTB_FEED")
+src-git overthebox $OTB_FEED
 EOF
 
-cat > source/.config <<EOF
+cat > openwrt/.config <<EOF
 $(for i in $OTB_TARGET $OTB_CONFIG; do cat "config/$i"; done)
 CONFIG_IMAGEOPT=y
 CONFIG_VERSIONOPT=y
@@ -81,7 +59,7 @@ EOF
 
 echo "Building for the target $OTB_TARGET"
 
-cd source
+cd openwrt
 
 cp .config .config.keep
 scripts/feeds clean
